44d3afd788c2c9b6ec0731c607ea8365186aac40
Fz-TvT <wulicfz@163.com>
2026-06-12 09:12:11 +0800

    modify test file

--- ucm/store/yuanrong/connector.py
+++ ucm/store/yuanrong/connector.py
@@ -63,7 +63,7 @@ class UcmYuanrongStore(UcmKVStoreBaseV1):
 
         # Initialize HeteroClient and connect to yuanrong worker
         self.client = self._HeteroClient(self.host, self.port)
-        ret = self.client.init()
+        ret = self.client
 
         if ret != 0:
             raise RuntimeError(
